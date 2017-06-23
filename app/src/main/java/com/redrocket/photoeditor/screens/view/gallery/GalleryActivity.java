49bed457a6c0cca6d507e723f02f1a5356562b70
Bob <bob@bob.com>
2017-06-23 08:00:56 +0700

    Init commit

---
+++ app/src/main/java/com/redrocket/photoeditor/screens/view/gallery/GalleryActivity.java
@@ -0,0 +1,16 @@
+package com.redrocket.photoeditor.screens.view.gallery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.redrocket.photoeditor.R;
+
+public class GalleryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery);
+
+    }
+}
---
+++ app/src/main/java/com/redrocket/photoeditor/screens/view/workspace/WorkspaceActivity.java
@@ -0,0 +1,15 @@
+package com.redrocket.photoeditor.screens.view.workspace;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.redrocket.photoeditor.R;
+
+public class WorkspaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workspace);
+    }
+}
